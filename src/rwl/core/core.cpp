9202da935c5cd7a5bd9af9e5e293e3aadb2bc637
Mayank Shigaonker <shigaonkerMayank@gmail.com>
2021-06-05 16:24:11 +0530

    Now connection to X Server is check when crreated

--- src/rwl/core/core.cpp
+++ src/rwl/core/core.cpp
@@ -2,7 +2,16 @@
 
 namespace rwl {
 #if RWL_PLATFORM == LINUX
-  xcb_connection_t *core::c = xcb_connect(nullptr, nullptr);
+  xcb_connection_t *core::c = ([] {
+    xcb_connection_t *c = xcb_connect(nullptr, nullptr);
+
+    if (int err = xcb_connection_has_error(c)) {
+      printf("Couldn't create connection to X Server. Error Code: %d", err);
+      exit(EXIT_FAILURE);
+    }
+
+    return c;
+  })();
   xcb_screen_t *core::scr = xcb_setup_roots_iterator(xcb_get_setup(c)).data;
 #endif
 
