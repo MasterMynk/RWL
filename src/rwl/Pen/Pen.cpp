d3ab479609d84662cac9c06fcbf0cc54d90333ce
Mayank Shigaonker <shigaonkerMayank@gmail.com>
2021-06-07 14:03:36 +0530

    Added Line Style option

--- src/rwl/Pen/Pen.cpp
+++ src/rwl/Pen/Pen.cpp
@@ -1,18 +1,26 @@
 #include "Pen.hpp"
 #include "rwl/core/core.hpp"
 
+/*
+ * All Functions defined here are in the order they're declared in the hpp file
+ */
+
 #define CREATE_PEN_MASK                                                        \
   uint32_t penMask[] = {this->m_fgColor.m_color, this->m_bgColor.m_color,      \
-                        this->m_lineWidth, 0}
+                        this->m_lineWidth,                                     \
+                        static_cast<uint32_t>(this->m_lineStyle), 0}
 #define LOGGING_HELPER(x)                                                      \
   impl::log("Pen ", (x), ": fg color --> ", this->m_fgColor.colorToStr(),      \
             ", bg color --> ", this->m_bgColor.colorToStr(),                   \
-            " and line width --> ", this->m_lineWidth)
+            ", line width --> ", this->m_lineWidth, " and line style --> ",    \
+            static_cast<uint32_t>(this->m_lineStyle))
 
 namespace rwl {
   uint32_t Pen::s_valueMask = XCB_GC_FOREGROUND | XCB_GC_BACKGROUND |
-                              XCB_GC_LINE_WIDTH | XCB_GC_GRAPHICS_EXPOSURES;
+                              XCB_GC_LINE_WIDTH | XCB_GC_LINE_STYLE |
+                              XCB_GC_GRAPHICS_EXPOSURES;
 
+  /*************************** Private Functions ***************************/
   void Pen::create() {
 #if RWL_PLATFORM == LINUX
     CREATE_PEN_MASK;
@@ -34,31 +42,36 @@ namespace rwl {
 #endif
   }
 
-  Pen::Pen(Pen &&other)
+  /******************************* Constructors *******************************/
+  Pen::Pen(Pen &&other) // Move Constructor
       : m_pen(other.m_pen), m_fgColor(other.m_fgColor),
-        m_bgColor(other.m_bgColor), m_lineWidth(other.m_lineWidth) {
+        m_bgColor(other.m_bgColor), m_lineWidth(other.m_lineWidth),
+        m_lineStyle(other.m_lineStyle) {
     other.m_pen = 0;
     LOGGING_HELPER("Moved");
   }
 
-  Pen::Pen(const Pen &other)
+  Pen::Pen(const Pen &other) // Copy Constructor
       : m_pen(xcb_generate_id(impl::core::conn)), m_fgColor(other.m_fgColor),
-        m_bgColor(other.m_bgColor), m_lineWidth(other.m_lineWidth) {
+        m_bgColor(other.m_bgColor), m_lineWidth(other.m_lineWidth),
+        m_lineStyle(other.m_lineStyle) {
     this->create();
   }
 
   Pen::Pen(const Color &fgColor, const Color &bgColor,
-           const uint32_t &lineWidth)
+           const uint32_t &lineWidth, const LineStyle &lineStyle) // Default
       : m_pen(xcb_generate_id(impl::core::conn)), m_fgColor(fgColor),
-        m_bgColor(bgColor), m_lineWidth(lineWidth) {
+        m_bgColor(bgColor), m_lineWidth(lineWidth), m_lineStyle(lineStyle) {
     this->create();
   }
 
+  /******************************** Operators ********************************/
   Pen &Pen::operator=(const Pen &other) {
     return this->setAllProps(other.m_fgColor, other.m_bgColor,
-                             other.m_lineWidth);
+                             other.m_lineWidth, other.m_lineStyle);
   }
 
+  /********************************* Setters *********************************/
   Pen &Pen::setFgColor(const Color &fgColor) {
     this->m_fgColor = fgColor;
     this->change();
@@ -68,7 +81,14 @@ namespace rwl {
 
   Pen &Pen::setLineWidth(const uint32_t &lineWidth) {
     this->m_lineWidth = lineWidth;
-    change();
+    this->change();
+
+    return *this;
+  }
+
+  Pen &Pen::setLineStyle(const LineStyle &lineStyle) {
+    this->m_lineStyle = lineStyle;
+    this->change();
 
     return *this;
   }
@@ -81,11 +101,21 @@ namespace rwl {
     return *this;
   }
 
+  Pen &Pen::setLineProps(const uint32_t &lineWidth,
+                         const LineStyle &lineStyle) {
+    this->m_lineWidth = lineWidth;
+    this->m_lineStyle = lineStyle;
+    this->change();
+
+    return *this;
+  }
+
   Pen &Pen::setAllProps(const Color &fgColor, const Color &bgColor,
-                        const uint32_t &lineWidth) {
+                        const uint32_t &lineWidth, const LineStyle &lineStyle) {
     this->m_fgColor = fgColor;
     this->m_bgColor = bgColor;
     this->m_lineWidth = lineWidth;
+    this->m_lineStyle = lineStyle;
     this->change();
 
     return *this;
--- src/rwl/Pen/Pen.hpp
+++ src/rwl/Pen/Pen.hpp
@@ -5,12 +5,19 @@
 
 namespace rwl {
   class Pen {
+  public:
+    enum class LineStyle : uint32_t {
+      Solid = XCB_LINE_STYLE_SOLID,
+      OnOff = XCB_LINE_STYLE_ON_OFF_DASH
+    };
+
   private:
 #if RWL_PLATFORM == LINUX
     xcb_gcontext_t m_pen;
     Color m_fgColor, m_bgColor;
     static uint32_t s_valueMask;
     uint32_t m_lineWidth;
+    LineStyle m_lineStyle;
 #endif
 
   private:
@@ -18,16 +25,27 @@ namespace rwl {
     void change();
 
   public:
+    /****************************** Constructor *******************************/
     Pen(Pen &&other);
     Pen(const Pen &other);
     Pen(const Color &fgColor = Color::Black,
-        const Color &bgColor = Color::Black, const uint32_t &lineWidth = 1);
+        const Color &bgColor = Color::Black, const uint32_t &lineWidth = 1,
+        const LineStyle &lineStyle = LineStyle::Solid);
 
+    /******************************* Operators ********************************/
     Pen &operator=(const Pen &other);
-    inline Pen &operator=(const Color &fgColor) {
+
+    inline Pen &operator=(const Color::ColorEnum &fgColor) {
       return this->setFgColor(fgColor);
     }
+    inline Pen &operator=(const uint32_t &lineWidth) {
+      return this->setLineWidth(lineWidth);
+    }
+    inline Pen &operator=(const LineStyle &lineStyle) {
+      return this->setLineStyle(lineStyle);
+    }
 
+    /******************************** Getters *********************************/
     inline const Color &getFgColor() const {
       impl::log("Returning fg color as ", this->m_fgColor.colorToStr());
       return this->m_fgColor;
@@ -40,12 +58,20 @@ namespace rwl {
       impl::log("Returning line width as ", this->m_lineWidth);
       return this->m_lineWidth;
     }
+    inline const LineStyle &getLineStyle() const {
+      impl::log("Returning line style as ",
+                static_cast<uint32_t>(this->m_lineStyle));
+      return this->m_lineStyle;
+    }
 
+    /******************************** Setters *********************************/
     Pen &setFgColor(const Color &fgColor);
     Pen &setLineWidth(const uint32_t &lineWidth);
+    Pen &setLineStyle(const LineStyle &lineStyle);
     Pen &setColors(const Color &fgColor, const Color &bgColor);
+    Pen &setLineProps(const uint32_t &lineWidth, const LineStyle &lineStyle);
     Pen &setAllProps(const Color &fgColor, const Color &bgColor,
-                     const uint32_t &lineWidth);
+                     const uint32_t &lineWidth, const LineStyle &lineStyle);
 
 #if RWL_PLATFORM == LINUX && RWL_DEBUG == 1
     inline const xcb_gcontext_t getP() const {
