e82e1b33e66cd6f9533fdcdb0108ca1d90d4517d
Mayank Shigaonker <shigaonkerMayank@gmail.com>
2021-06-10 17:11:51 +0530

    Added an option to specify the window border

--- include/rwl/Window/Window.hpp
+++ include/rwl/Window/Window.hpp
@@ -7,15 +7,14 @@ namespace rwl {
   private:
     void create();
     const WinComm &m_parent;
+    const uint16_t m_borderWidth;
 
   public:
     explicit Window(Window &&other);
     explicit Window(const Window &other);
     Window(const Pos &pos = {0, 0}, const Dim &dim = {640, 480},
            const Color &bgColor = Color::White,
-           const impl::WinComm &parent = root);
-    // Window(const Pos &pos, const Dim &dim, const Color &bgColor,
-    //        const WinComm &parent);
+           const impl::WinComm &parent = root, const uint16_t &borderWidth = 1);
 
     inline const WinComm &getParent() const { return m_parent; }
 
--- src/Window/Window.cpp
+++ src/Window/Window.cpp
@@ -6,11 +6,12 @@ namespace rwl {
 #if RWL_PLATFORM == LINUX
     uint32_t props[2] = {this->m_bgColor.m_color, XCB_EVENT_MASK_EXPOSURE};
 
-    xcb_create_window(
-        impl::core::conn, impl::core::scr->root_depth, m_win, m_parent.m_win,
-        this->m_pos.x, this->m_pos.y, this->m_dim.width, this->m_dim.height, 3,
-        XCB_WINDOW_CLASS_INPUT_OUTPUT, impl::core::scr->root_visual,
-        XCB_CW_BACK_PIXEL | XCB_CW_EVENT_MASK, &props);
+    xcb_create_window(impl::core::conn, impl::core::scr->root_depth, m_win,
+                      m_parent.m_win, this->m_pos.x, this->m_pos.y,
+                      this->m_dim.width, this->m_dim.height,
+                      this->m_borderWidth, XCB_WINDOW_CLASS_INPUT_OUTPUT,
+                      impl::core::scr->root_visual,
+                      XCB_CW_BACK_PIXEL | XCB_CW_EVENT_MASK, &props);
     impl::log("Created a window with window Id: ", m_win);
 #endif
   }
@@ -18,22 +19,19 @@ namespace rwl {
   /******************************* Constructors *******************************/
   Window::Window(Window &&other)
       : WinComm(other.m_win, other.m_pos, other.m_dim, other.m_bgColor),
-        m_parent(other.m_parent) {
+        m_parent(other.m_parent), m_borderWidth(other.m_borderWidth) {
     impl::log("Moved a window with window Id: ", m_win);
     other.m_win = 0;
   }
 
   Window::Window(const Window &other)
-      : WinComm(xcb_generate_id(impl::core::conn), other.m_pos, other.m_dim,
-                other.m_bgColor),
-        m_parent(other.m_parent) {
-    create();
-  }
+      : Window(other.m_pos, other.m_dim, other.m_bgColor, other.m_parent,
+               other.m_borderWidth) {}
 
   Window::Window(const Pos &pos, const Dim &dim, const Color &bgColor,
-                 const WinComm &parent)
+                 const WinComm &parent, const uint16_t &borderWidth)
       : WinComm(xcb_generate_id(impl::core::conn), pos, dim, bgColor),
-        m_parent(parent) {
+        m_parent(parent), m_borderWidth(borderWidth) {
     create();
   }
 
